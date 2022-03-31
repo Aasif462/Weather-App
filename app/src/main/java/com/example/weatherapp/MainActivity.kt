f1268ca2c765e4687f69cac9218439d0a66bbfa8
Aasif <qadriaasif76@gmail.com>
2022-03-31 17:11:14 +0530

    Location Permission Added

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,11 +1,115 @@
 package com.example.weatherapp
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.location.Location
+import android.location.LocationManager
+import android.location.LocationRequest
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
+import android.widget.Toast
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mFusedLocationClient:FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if(!isLocationEnabled()){
+            Toast.makeText(applicationContext, "Please Turn on You GPS", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(intent)
+        }
+        else{
+            Dexter.withContext(this).withPermissions(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+                .withListener( object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        if (report!!.areAllPermissionsGranted()) {
+                            requestNewLocationData()
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        p0: MutableList<PermissionRequest>?,
+                        p1: PermissionToken?
+                    ) {
+                        showRationalDialog()
+                    }
+                }).onSameThread().check()
+        }
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this@MainActivity)
+    }
+
+    private fun isLocationEnabled():Boolean{
+        val locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun showRationalDialog(){
+        val dialog = AlertDialog.Builder(this)
+        dialog.setMessage("It Looks Like You have turned off Permission required"+
+                "For this Feature. It can be enabled Under the "+"Application Settings")
+        dialog.setPositiveButton("GO TO SETTINGS"){
+                dialog_,which ->
+            try {
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package",packageName ,null)
+                intent.data = uri
+                startActivity(intent)
+            }
+            catch (e: ActivityNotFoundException){
+                e.printStackTrace()
+            }
+        }
+        dialog.setNegativeButton("Cancel"){
+                dialog,_ ->
+            dialog.dismiss()
+        }
+        dialog.show()
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestNewLocationData(){
+        val mLocationRequest = com.google.android.gms.location.LocationRequest()
+        mLocationRequest.priority = LocationRequest.QUALITY_HIGH_ACCURACY
+        mLocationRequest.interval = 1000
+        mLocationRequest.numUpdates = 1
+
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest , mLocationCallback , Looper.myLooper()!!)
+    }
+
+    private val mLocationCallback = object: LocationCallback(){
+        override fun onLocationResult(locationResult: LocationResult) {
+            super.onLocationResult(locationResult)
+            val mLastLocation: Location = locationResult.lastLocation
+            val latitude = mLastLocation.latitude
+
+            val longitude = mLastLocation.longitude
+
+
+        }
     }
 }
